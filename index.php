e6c8454fc10e4fd03971a8a8feee38a7877d3ac0
Vladimir Gontar <volodyawork@gmail.com>
2014-04-22 10:43:18 +0300

    finish

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <?php
-    echo "Hello World!!!!!!!!!!!!\n\r";
+echo "Hello!\n\r";
+/**
+ * @param $str
+ * @return mixed - array - statistics result
+ */
+function string_stat($str)
+{
+    $res =[];
+    $len = strlen($str);
+    for ($i=0; $i<$len;$i++){
+        if (isset($res[$str[$i]]))
+            $res[$str[$i]]++;
+        else $res[$str[$i]] =1;
+    }
+    return $res;
+}
+
+parse_str(implode('&', array_slice($argv, 1)), $_GET);
+if (isset($_GET['s']) && ($_GET['s'] != '')) {
+    print_r(string_stat($_GET['s']));
+} else {
+    echo "Input please parameter 's' in console. Ex.: \n\r";
+    echo 'php -f index.php s="it is string"' . "\n\r";
+}
