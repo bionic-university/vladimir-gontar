73097621ff887a5f371aed064e131ef754acedb4
Vladimir Gontar <volodyawork@gmail.com>
2014-05-14 21:28:44 +0300

    exam fix

--- src/BionicUniversity/VladimirGontar/exam/Resizer.php
+++ src/BionicUniversity/VladimirGontar/exam/Resizer.php
@@ -102,6 +102,6 @@ class Resizer extends AbstractResizer implements ImageInterface
      */
     public function getCenterCoordinates()
     {
-        return array(ceil($this->width), ceil($this->height));
+        return array(ceil($this->width/2), ceil($this->height/2));
     }
 } 
