88e5a763d331c329e4dc6a770ec4cf742c90665f
Vladimir Gontar <volodyawork@gmail.com>
2014-11-08 14:16:24 +0200

    add subscriber

---
+++ phpsymfony/src/BionicUniversity/Bundle/BlogBundle/Subscriber/BlogSubscriber.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BionicUniversity\Bundle\BlogBundle\Subscriber;
+
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class BlogSubscriber implements EventSubscriberInterface{
+    static public function getSubscribedEvents()
+    {
+        return array(
+            'kernel.response' => 'onKernelResponse',
+            'kernel.request'     => 'onKernelRequest',
+        );
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        echo('onKernelRequest'."\r");
+
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        echo('onKernelResponse');
+        exit();
+
+    }
+
+}
